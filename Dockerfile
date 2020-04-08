0e6a4cdbbbdafa7455935afb2a245dca1b1e0102
snul <simon.underhill@gmail.com>
2020-04-08 23:41:26 +0100

    add basic setup for app - wip

--- Dockerfile
+++ Dockerfile
@@ -12,3 +12,10 @@ WORKDIR $GOPATH/src/github.com/hashicorp/terraform
 RUN git clone https://github.com/hashicorp/terraform.git ./ && \
     git checkout v${TERRAFORM_VERSION} && \
     /bin/bash scripts/build.sh
+
+WORKDIR /srv
+COPY . .
+RUN pip install -r ./requirements.txt
+RUN ["chmod", "+x", "./app_docker.sh"]
+EXPOSE 3100
+CMD ["/bin/bash", "./app_docker.sh"]
---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+import config
+import logging
+
+print("DEBUG: {}".format(os.environ['DEBUG']))
+
+# Get theapplication instance
+connex_app = config.connex_app
+
+# connect logging between gunicorn and Flask
+#gunicorn_logger = logging.getLogger("gunicorn.error")
+gunicorn_logger = logging.getLogger("gunicorn.info")
+connex_app.app.logger.handlers = gunicorn_logger.handlers
+connex_app.app.logger.setLevel(gunicorn_logger.level)
+
+# Read the swagger.yml file to configure the endpoints
+#connex_app.add_api('swagger.yml', strict_validation=True)
+connex_app.add_api('swagger.yml', strict_validation=False)
+
+if __name__ == "__main__":
+    connex_app.run(port=3100, debug=os.environ['DEBUG'])
