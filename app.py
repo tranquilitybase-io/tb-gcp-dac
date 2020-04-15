147feec2833ab0c98f0f9378e27313362a57c209
snul <simon.underhill@gft.com>
2020-04-15 10:05:16 +0100

    tidy

--- app.py
+++ app.py
@@ -10,13 +10,13 @@ print("DEBUG: {}".format(os.environ['DEBUG']))
 connex_app = config.connex_app
 
 # connect logging between gunicorn and Flask
-#gunicorn_logger = logging.getLogger("gunicorn.error")
+# gunicorn_logger = logging.getLogger("gunicorn.error")
 gunicorn_logger = logging.getLogger("gunicorn.info")
 connex_app.app.logger.handlers = gunicorn_logger.handlers
 connex_app.app.logger.setLevel(gunicorn_logger.level)
 
 # Read the swagger.yml file to configure the endpoints
-#connex_app.add_api('swagger.yml', strict_validation=True)
+# connex_app.add_api('swagger.yml', strict_validation=True)
 connex_app.add_api('swagger.yml', strict_validation=False)
 
 if __name__ == "__main__":
