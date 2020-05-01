628935a09296c71296420a2ec36915c0675614a7
snul <simon.underhill@gft.com>
2020-05-01 11:52:56 +0100

    add celery check to main app. will remove later

--- celery_worker.py
+++ celery_worker.py
@@ -1,7 +1,6 @@
 import logging
 
 from celery import signals
-from celery.signals import after_setup_logger
 
 from config import get_celery
 from gcpdac.solution_terraform import run_terraform
@@ -13,7 +12,7 @@ log_format = '[%(asctime)s] [%(name)s] [%(levelname)s]: %(message)s'
 logging.basicConfig(level=logging.INFO, format=log_format)
 
 @celery_app.task()
-def add(a, b):
+def add_two_numbers(a, b):
     logger.debug("add %s + %s", a, b)
     return a + b
 
@@ -30,22 +29,6 @@ def destroy_solution_task(solutionDetails):
     logger.debug("destroy_solution_task")
     response = run_terraform(solutionDetails, "destroy")
     return response
-
-
-@after_setup_logger.connect
-def setup_loggers(logger, *args, **kwargs):
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-    # FileHandler
-    fh = logging.FileHandler('logs.log')
-    fh.setFormatter(formatter)
-    logger.addHandler(fh)
-
-    # # SysLogHandler
-    # slh = logging.handlers.SysLogHandler(address=('logsN.papertrailapp.com', '...'))
-    # slh.setFormatter(formatter)
-    # logger.addHandler(slh)
-
 
 # DON'T REMOVE - added to fix celery logging error
 @signals.setup_logging.connect
--- gcpdac/celerycheck.py
+++ gcpdac/celerycheck.py
@@ -1,17 +1,15 @@
 from flask import request, jsonify
 
 import config
-from celery_worker import add
+from celery_worker import add_two_numbers
 logger = config.logger
 
 def add_together():
     logger.info("ADD TOGETHER")
-    result = add.delay(23, 42)
-    result.wait()
 
     x = int(request.args.get("x", 1))
     y = int(request.args.get("y", 2))
-    res = add.apply_async((x, y))
+    res = add_two_numbers.apply_async((x, y))
     context = {"id": res.task_id, "x": x, "y": y}
     result = "add((x){}, (y){})".format(context['x'], context['y'])
     goto = "{}".format(context['id'])
@@ -20,5 +18,5 @@ def add_together():
 
 def add_together_result(taskid):
     logger.info("ADD TOGETHER RESULT %s",format(taskid))
-    retval = add.AsyncResult(taskid).get(timeout=1.0)
+    retval = add_two_numbers.AsyncResult(taskid).get(timeout=1.0)
     return {"sum": str(repr(retval))}
