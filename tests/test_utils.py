c0284df7ea73184fcd0a5713ffa51dd97388ada7
snul <simon.underhill@gft.com>
2020-05-04 11:33:05 +0100

    add label tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,4 @@
+import unittest
 from unittest import TestCase
 
 from gcpdac.utils import labellize
@@ -5,9 +6,20 @@ from gcpdac.utils import labellize
 
 class Test(TestCase):
     def test_labellize(self):
-        assert labellize("abc") == "abc"
-        assert labellize("ab c") == "ab-c"
-        assert labellize("ab&c") == "ab-c"
-        assert labellize("ab_c") == "ab_c"
-        assert labellize("ab-c") == "ab-c"
-        assert labellize("ABC") == "abc"
+        # google label rules here - https://cloud.google.com/compute/docs/labeling-resources
+        self.assertEqual("abc", labellize("abc"))
+        self.assertEqual("ab-c", labellize("ab c"))
+        self.assertEqual("ab-c", labellize("ab&c"))
+        self.assertEqual("ab_c", labellize("ab_c"))
+        self.assertEqual("ab-c", labellize("ab-c"))
+        self.assertEqual("abc", labellize("ABC"))
+        self.assertEqual("a123", labellize("123"))
+        self.assertEqual("a-123", labellize("-123"))
+        self.assertEqual("a-abc", labellize("-abc"))
+        self.assertEqual("a_123", labellize("_123"))
+        self.assertEqual("abcdefghijklimnopqrstuvwxyz-0123456789_abcdefghijklimnopqrstuvw",
+                         labellize("abcdefghijklimnopqrstuvwxyz-0123456789_abcdefghijklimnopqrstuvwxyz"))
+
+
+if __name__ == '__main__':
+    unittest.main()
