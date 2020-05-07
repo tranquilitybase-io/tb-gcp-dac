757bdf0f59a2c2b128fb1c08ac9588942f14cf99
karwootang-gft <karwoo.tang@gft.com>
2020-05-07 20:48:41 +0100

    codacy

--- mocks/mock_gcp_dac/gcpdac/health.py
+++ mocks/mock_gcp_dac/gcpdac/health.py
@@ -5,7 +5,7 @@ Health check: Very basic health check, can extend in future
 from gcpdac.extendedSchemas import HealthSchema
 
 def check():
-    """
+    """.
     :return: 200 on success.
     """
 
