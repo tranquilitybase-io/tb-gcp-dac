4cad0f074cee97c1cdc5044c626f83188d7b2e1d
karwootang-gft <karwoo.tang@gft.com>
2020-05-22 15:33:51 +0100

    remove read_config_map not being used in the mock

--- mocks/mock_gcp_dac/config.py
+++ mocks/mock_gcp_dac/config.py
@@ -13,24 +13,3 @@ counter = itertools.count()
 
 app = connex_app.app
 ma = Marshmallow(app)
-
-
-ec_config = None
-
-
-def read_config_map():
-    # Returns the EC configuration as a dictionary
-
-    try:
-        with open("gcpdac/ec-config.yaml", 'r') as stream:
-            try:
-                ec_config = yaml.safe_load(stream)
-                return ec_config
-            except yaml.YAMLError as exc:
-                logger.exception("Failed to parse EC YAML after successfully opening - {}".format(exc))
-                raise
-    except Exception:
-        print("Failed to load EC YAML file")
-        raise
-
-    return ec_config
