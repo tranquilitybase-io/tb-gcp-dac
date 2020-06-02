04fc9be09d392c3f4f5c80be57befad09c03ca3e
snul <simon.underhill@gft.com>
2020-06-02 13:19:27 +0100

    add checks for folder values, long folder name test

--- tests/gcp_tests/test_folder.py
+++ tests/gcp_tests/test_folder.py
@@ -1,3 +1,4 @@
+import json
 import unittest
 from time import sleep
 from celery import states
@@ -6,26 +7,40 @@ from tests.gcp_tests.folder_utils import create_folder_task, delete_folder_task,
 
 validParentFolderId = "940168182397"
 validFolderName = "TEST_FOLDER"
+longFolderName = "abcdefghijkl12345nopqrstuvwxyz"  # 31 chars
 
 class FolderTest(unittest.TestCase):
     def test_folder(self):
 
-        print("Creating a folder")
+        payload = self.create_folder(folder_name=validFolderName, parent_folder_id=validParentFolderId)
+        folder = json.loads(payload)
+        display_name = folder["folder"]["value"]["display_name"]
+        folder_id = folder["folder"]["value"]["id"]
+        return_parent_folder_id = folder["folder"]["value"]["parent"]
+        self.assertEqual(validFolderName, display_name)
+        self.assertEqual(validParentFolderId, self.replace_folders(return_parent_folder_id))
+        self.assertTrue(int(self.replace_folders(folder_id)) > 0)
 
-        taskid = create_folder_task(validFolderName, validParentFolderId)
-        print("Celery task id {}".format(taskid))
-        status = ''
-        while (status != states.SUCCESS and status != states.FAILURE):
-            print("Checking task {}".format(taskid))
-            status, payload = create_folder_task_result(taskid)
-            print('Status {}'.format(status))
-            print('Payload {}'.format(payload))
-            sleep(10)
+        self.delete_folder(folder_name=validFolderName)
 
-        self.assertEqual(states.SUCCESS, status)
+    def test_long_folder_name(self):
+        payload = self.create_folder(folder_name=longFolderName, parent_folder_id=validParentFolderId)
+        folder = json.loads(payload)
+        display_name = folder["folder"]["value"]["display_name"]
+        folder_id = folder["folder"]["value"]["id"]
+        return_parent_folder_id = folder["folder"]["value"]["parent"]
+        self.assertEqual(longFolderName[0:30], display_name)
+        self.assertEqual(validParentFolderId, self.replace_folders(return_parent_folder_id))
+        self.assertTrue(int(self.replace_folders(folder_id)) > 0)
 
+        self.delete_folder(folder_name=longFolderName)
+
+    def replace_folders(self, folder_id):
+        return folder_id.replace("folders/", "")
+
+    def delete_folder(self, folder_name):
         print("Deleting a folder")
-        taskid = delete_folder_task(validFolderName)
+        taskid = delete_folder_task(folder_name)
         print("Celery task id {}".format(taskid))
         status = ''
         while (status != states.SUCCESS and status != states.FAILURE):
@@ -34,8 +49,21 @@ class FolderTest(unittest.TestCase):
             print('Status {}'.format(status))
             print('Payload {}'.format(payload))
             sleep(10)
-
         self.assertEqual(states.SUCCESS, status)
+
+    def create_folder(self, folder_name, parent_folder_id):
+        print("Creating a folder")
+        taskid = create_folder_task(folder_name, parent_folder_id)
+        print("Celery task id {}".format(taskid))
+        status = ''
+        while (status != states.SUCCESS and status != states.FAILURE):
+            print("Checking task {}".format(taskid))
+            status, payload = create_folder_task_result(taskid)
+            print('Status {}'.format(status))
+            print('Payload {}'.format(payload))
+            sleep(10)
+        self.assertEqual(states.SUCCESS, status)
+        return payload
 
 
 if __name__ == '__main__':
