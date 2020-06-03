3d623dc888a67a3082be5e8ab030fe7f1277e518
snul <simon.underhill@gft.com>
2020-06-03 09:10:46 +0100

    remove unnecessary comments

--- tests/gcp_tests/folder_utils.py
+++ tests/gcp_tests/folder_utils.py
@@ -10,14 +10,11 @@ headers = {'Content-Type': 'application/json' }
 
 def create_folder_task(folderName, parentFolderId):
 
-    #Test POST Then GET
-    # Body
     url = 'http://localhost:3100/api/folder_async'
     payload = {"folder": {"folderName": folderName, "parentFolderId": parentFolderId}}
-    # convert dict to json by json.dumps() for body data.
+
     resp = requests.post(url, headers=headers, data=json.dumps(payload,indent=4))
 
-    # Validate response headers and body contents, e.g. status code.
     resp_json = resp.json()
     task_id = resp_json['taskid']
 
@@ -26,10 +23,8 @@ def create_folder_task(folderName, parentFolderId):
 def create_folder_task_result(taskId):
 
     url = 'http://localhost:3100/api/folder_async/result/create/{}'.format(taskId)
-    # convert dict to json by json.dumps() for body data.
     resp = requests.get(url, headers=headers)
 
-    # Validate response headers and body contents, e.g. status code.
     resp_json = resp.json()
     status = resp_json['status']
     payload = resp_json.get('payload', None)
@@ -38,12 +33,9 @@ def create_folder_task_result(taskId):
 
 def delete_folder_task(folderName):
 
-    # Body
     url = 'http://localhost:3100/api/folder_async/{}'.format(folderName)
-    # convert dict to json by json.dumps() for body data.
     resp = requests.delete(url, headers=headers)
 
-    # Validate response headers and body contents, e.g. status code.
     resp_json = resp.json()
     task_id = resp_json['taskid']
 
@@ -52,10 +44,8 @@ def delete_folder_task(folderName):
 def delete_folder_task_result(taskId):
 
     url = 'http://localhost:3100/api/folder_async/result/delete/{}'.format(taskId)
-    # convert dict to json by json.dumps() for body data.
     resp = requests.get(url, headers=headers)
 
-    # Validate response headers and body contents, e.g. status code.
     resp_json = resp.json()
     status = resp_json['status']
     payload = resp_json.get('payload', None)
