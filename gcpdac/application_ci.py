84e428aac97cd0ed59ebb1bc5438e0cd2a053acd
snul <simon.underhill@gft.com>
2020-06-11 10:12:52 +0100

    add api methods/celery tasks for applications add script to create repo

---
+++ gcpdac/application_ci.py
@@ -0,0 +1,17 @@
+
+
+def create_application(applicationdata):
+    # create terraform vars file
+    # check vars file into repo
+    # check repo for result file created by Jenkins CI
+
+    # OR
+
+    # create terraform vars file
+    # call Jenkins CI directly with vars file
+
+    return -1
+
+
+def delete_application(applicationdata):
+    return -1
--- gcpdac/celery_tasks.py
+++ gcpdac/celery_tasks.py
@@ -4,6 +4,7 @@ from config import get_celery
 from gcpdac.folder_terraform import create_folder, delete_folder
 from gcpdac.local_logging import get_logger
 from gcpdac.solution_terraform import create_solution, delete_solution
+from gcpdac.application_ci import create_application, delete_application
 
 celery_app = get_celery()
 
@@ -50,4 +51,27 @@ def delete_folder_task(self, folderDetails):
         self.update_state(state=states.SUCCESS)
     return response
 
+
+@celery_app.task(bind=True)
+def deploy_application_task(self, applicationDetails):
+    logger.debug("deploy_application_task")
+    response = create_application(applicationDetails)
+    return_code = response.get("return_code")
+    if (return_code) != 0:
+        self.update_state(state=states.FAILURE)
+    else:
+        self.update_state(state=states.SUCCESS)
+    return response
+
+
+@celery_app.task(bind=True)
+def destroy_application_task(self, applicationDetails):
+    logger.debug("destroy_application_task")
+    response = delete_application(applicationDetails)
+    return_code = response.get("return_code")
+    if (return_code) != 0:
+        self.update_state(state=states.FAILURE)
+    else:
+        self.update_state(state=states.SUCCESS)
+    return response
 
