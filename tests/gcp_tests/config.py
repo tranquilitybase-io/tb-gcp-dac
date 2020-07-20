2ccf9541e45bd3f9c1492dee38bd6d286a7d3424
snul <simon.underhill@gft.com>
2020-07-20 10:40:08 +0100

    update test config

--- tests/gcp_tests/config.py
+++ tests/gcp_tests/config.py
@@ -3,5 +3,23 @@ workspaceProjectId = "workspace-j8u3pn-ksjs726s"  # a solution workspace project
 deploymentProjectId = "development-j8u3pn-ksjs726s" #  a solution environment project id
 activator_git_url = "https://source.developers.google.com/p/workspace-93b7sc-ksjs726s/r/activator-testapp" # git url for an external activator
 base_folder_id = "302197932093" # tranquility base folder
-# deploymentProjectId = None
+team_members = [
+    {
+        "role": {
+            "name": "admin",
+            "cloudIdentityGroup": "ADD HERE",
+            "id": 1,
+            "description": "eagle console admin role"
+        },
+        "user": {
+            "lastName": "Underhill",
+            "email": "simon.underhill@gft.com",
+            "id": 1,
+            "firstName": "Simon",
+            "isAdmin": False,
+            "showWelcome": True
+        },
+        "id": 2
+    }
+]# deploymentProjectId = None
 # workspaceProjectId = None
