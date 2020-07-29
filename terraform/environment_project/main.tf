5fd7fb49e6a87b1182945996c5daeba2f9972b90
snul <simon.underhill@gft.com>
2020-07-29 13:31:54 +0100

    fix tf

--- terraform/environment_project/main.tf
+++ terraform/environment_project/main.tf
@@ -48,7 +48,8 @@ resource "google_folder_iam_binding" "folder_member" {
 }
 
 resource "google_compute_shared_vpc_service_project" "service" {
-  count = length(var.shared_vpc_host_project) > 0 ? 1 : 0
+//  count = length(var.shared_vpc_host_project) > 0 ? 1 : 0
+  count = length(var.environments)
   host_project    = var.shared_vpc_host_project
   service_project = google_project.environment_project[count.index].project_id
   depends_on = [google_project.environment_project]
