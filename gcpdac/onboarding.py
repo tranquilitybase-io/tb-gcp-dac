1fc5df59bc429b5974b78620f8fba31994f55b3a
Geoffrey Pell <>
2020-11-18 16:10:48 +0000

    #98 Initialize internal activator repo - changes from iteration under testing

--- gcpdac/onboarding.py
+++ gcpdac/onboarding.py
@@ -4,6 +4,8 @@ import git
 import yaml
 import config
 from gcloud import resource_manager
+
+from gcpdac.path_utils import file_exists
 from gcpdac.shell_utils import create_and_save
 
 logger = config.logger
@@ -13,42 +15,49 @@ def get_client():
     return resource_manager.Client()
 
 
-def list_projects():
-    client = get_client()
-    for project in client.list_projects():
-        print(project)
-    return client.list_projects()
-
-
 def get_ec_config():
-    with open('/app/ec-config.yaml', 'rb') as f:
-        yaml_variables = yaml.load(f.read())
+    # TODO: file_path needs a centralised config
+    file_path = 'ec-config.yaml'
+
+    if not file_exists(file_path):
+        raise Exception("file not found")
+
+    with open(file_path, 'rb') as f:
+        yaml_variables = yaml.load(f.read(), yaml.Loader)
+
     return yaml_variables
 
 
 def get_destination_project():
-    project_list = list_projects()
-    project_to_prefix = "shared-ec-"
-    result = ""
+    # TODO: project_prefix needs a centralised config
+    project_prefix = "shared-ec-"
+
     yaml_variables = get_ec_config()
     tb_discriminator = yaml_variables['tb_discriminator']
-    for p in project_list:
-        result = p.match(project_to_prefix) + tb_discriminator
+    result = project_prefix + tb_discriminator
+
     return result
 
 
-def get_repo_uri(repo_json):
-    # json_string = '{"activatorName": "tb-gcp-hpc-activator", "repoURL":"someurl", "tagName": "sometag"}'
-    parsed_json = json.loads(repo_json)
-    gcp_repo = '{"repository": ""}'
-    # git clone <repo_url> --branch <tag_name> --single-branch
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        repo_url = parsed_json['repoURL']
-        local_repo = git.Repo.clone_from(repo_url, tmpdirname)
-        local_repo.checkout(parsed_json['tagName'])
-        flag = create_and_save(local_repo, get_destination_project())
-        if flag:
-            gcp_repo["repository"] = local_repo
-            logger.info("return json : ", gcp_repo)
+def clone_repo_locally(gitDetails):
+    try:
+        repo_url = gitDetails['repoURL']
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            local_repo = git.Repo.clone_from(repo_url, tmpdirname, no_checkout=True)
+            local_repo.git.checkout(gitDetails['tagName'])
+            return local_repo
+    except Exception:
+        raise Exception("Error cloning repository")
 
-    return json.dumps(gcp_repo)
+
+def get_repo_uri(gitDetails):
+    try:
+        local_repo = clone_repo_locally(gitDetails)
+        create_and_save(local_repo, get_destination_project())
+        gcp_repo = {'repository': local_repo}
+        payload = json.dumps(str(gcp_repo))
+        return {'status': 201, "payload": payload}
+    except Exception as ex:
+        logger.debug(ex)
+
+    return {'status': 500, "message": "Exception encountered"}
