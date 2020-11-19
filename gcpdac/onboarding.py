8a06a12447e49ef05a2b1a873e0b983609ff4cd9
Geoffrey Pell <>
2020-11-19 13:35:47 +0000

    #98 Initialize internal activator repo - changes from iteration under testing

--- gcpdac/onboarding.py
+++ gcpdac/onboarding.py
@@ -34,40 +34,39 @@ def get_destination_project():
     return project_prefix + tb_discriminator
 
 
-def clone_repo_locally(git_details):
+def clone_repo_locally(gitDetails):
     try:
-        repo_url = git_details['repoURL']
+        repo_url = gitDetails['repoURL']
         with tempfile.TemporaryDirectory() as tmpdirname:
             local_repo = git.Repo.clone_from(repo_url, tmpdirname, no_checkout=True)
-            local_repo.git.checkout(git_details['tagName'])
+            local_repo.git.checkout(gitDetails['tagName'])
             return local_repo
     except Exception:
         raise Exception("Error cloning repository")
 
 
-def get_repo_uri(git_details):
+def get_repo_uri(gitDetails):
     try:
         destination_project = get_destination_project()
-        local_repo = clone_repo_locally(git_details)
+        local_repo = clone_repo_locally(gitDetails)
         create_and_save(local_repo, destination_project)
         gcp_repo = json_builder(destination_project, local_repo)
         payload = json.dumps(str(gcp_repo))
-        return {'status': 201, "payload": payload}
+        return payload, 201
     except Exception as ex:
         logger.debug(ex)
 
-    return {'status': 500, "message": "Exception encountered"}
+    return "Exception encountered", 500
 
 
 def json_builder(project_id, local_repo):
     url_prefix = "https://source.cloud.google.com/"
-    dict_repo = '{"repo_name": "", "project_id": "", "head_link": "", "path_link": "", "browser_link": "", ' \
-                '"git_clone": "", "cloud_sdk": ""} '
-    dict_repo["repo_name"] = local_repo
-    dict_repo["project_id"] = project_id
-    dict_repo["head_link"] = url_prefix + project_id + "/" + local_repo + "/+/master:"
-    dict_repo["path_link"] = project_id + "/" + local_repo + "/master//"
-    dict_repo["browser_link"] = url_prefix + project_id + "/" + local_repo
-    dict_repo["git_clone"] = "git clone " + url_prefix + "p/" + project_id + "/r/" + local_repo
-    dict_repo["cloud_sdk"] = "gcloud source repos clone " + local_repo + " --project=" + project_id
+    dict_repo = {"repo_name": local_repo,
+                "project_id": project_id,
+                "head_link": url_prefix + project_id + "/" + local_repo + "/+/master:",
+                "path_link": project_id + "/" + local_repo + "/master//",
+                "browser_link": url_prefix + project_id + "/" + local_repo,
+                "git_clone": "git clone " + url_prefix + "p/" + project_id + "/r/" + local_repo,
+                "cloud_sdk": "gcloud source repos clone " + local_repo + " --project=" + project_id}
+
     return dict_repo
