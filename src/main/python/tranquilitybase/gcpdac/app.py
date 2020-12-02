a6960465bff97741a2f35d99b2994a913ac0a92c
Geoffrey Pell <Geoffrey.Pell@gft.com>
2020-12-02 09:38:35 +0000

    experimenting with project structure - docker paths

--- src/main/python/tranquilitybase/gcpdac/app.py
+++ src/main/python/tranquilitybase/gcpdac/app.py
@@ -1,21 +1,31 @@
 import logging
 import os
 
-import config
+basedir = os.path.abspath(os.path.dirname(__file__))
+print("basedir: {}".format(basedir))
+import connexion
+connex_app = connexion.App(__name__, specification_dir=basedir)
 
-print("DEBUG: {}".format(os.environ['DEBUG']))
+app = connex_app.app
 
-logger = config.logger
 
-connex_app = config.connex_app
 
-gunicorn_logger = logging.getLogger("gunicorn.info")
-connex_app.app.logger.handlers = gunicorn_logger.handlers
-connex_app.app.logger.setLevel(gunicorn_logger.level)
 
-connex_app.add_api('openapi.yml', strict_validation=True)
-
-celery = config.get_celery()
-
-if __name__ == "__main__":
-    connex_app.run(port=os.environ['APP_PORT'], debug=os.environ['DEBUG'])
+# from src.main.python.tranquilitybase.gcpdac import config
+#
+# print("DEBUG: {}".format(os.environ['DEBUG']))
+#
+# logger = config.logger
+#
+# connex_app = config.connex_app
+#
+# gunicorn_logger = logging.getLogger("gunicorn.info")
+# connex_app.app.logger.handlers = gunicorn_logger.handlers
+# connex_app.app.logger.setLevel(gunicorn_logger.level)
+#
+# connex_app.add_api('openapi.yml', strict_validation=True)
+#
+# celery = config.get_celery()
+#
+# if __name__ == "__main__":
+#     connex_app.run(port=os.environ['APP_PORT'], debug=os.environ['DEBUG'])
