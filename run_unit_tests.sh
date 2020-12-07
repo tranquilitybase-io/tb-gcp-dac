58f927b92057c5de23b27c986539c89a0a736130
Geoffrey Pell <Geoffrey.Pell@gft.com>
2020-12-07 15:57:17 +0000

    #189 iterating build issues

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -5,6 +5,6 @@ echo $(pwd)
 echo "-------------"
 ls
 echo "-------------"
-set-env name=PYTHONPATH::$(pwd)
+export PYTHONPATH=$(pwd)
 python3 -c "import sys; print(sys.path)"
 python3 -m unittest discover -s src/test/gcpdac_tests/unit/
