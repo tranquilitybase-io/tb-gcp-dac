82e3510d47f22f4362fde34714a1084ea7a58820
Geoffrey Pell <Geoffrey.Pell@gft.com>
2020-12-07 15:17:54 +0000

    #189 running all unit tests

--- src/test/gcpdac_tests/unit/test_config.py
+++ src/test/gcpdac_tests/unit/test_config.py
@@ -2,7 +2,7 @@ import unittest
 import os
 from unittest import TestCase
 
-from src.main.python.tranquilitybase.gcpdac.configuration.envhelper import EnvHelper
+from main.python.tranquilitybase.gcpdac.configuration.envhelper import EnvHelper
 
 
 class Utils_Test(TestCase):
