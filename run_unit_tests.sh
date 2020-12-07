8b356c4678a407376117e494f7bd9d1c95517857
Geoffrey Pell <Geoffrey.Pell@gft.com>
2020-12-07 16:11:39 +0000

    #189 iterating build issues

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -7,4 +7,7 @@ ls
 echo "-------------"
 export PYTHONPATH=$(pwd)
 python3 -c "import sys; print(sys.path)"
+echo "-------------"
+
+RUN pip install -r src/main/python/tranquilitybase/gcpdac/requirements.txt
 python3 -m unittest discover -s src/test/gcpdac_tests/unit/
