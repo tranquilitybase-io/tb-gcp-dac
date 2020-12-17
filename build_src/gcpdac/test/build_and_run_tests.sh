2fb2359df09cceec5c3087ae43c21f950326466c
Geoffrey Pell <Geoffrey.Pell@gft.com>
2020-12-17 18:33:52 +0000

    #431 debugging dockerfile path

--- build_src/gcpdac/test/build_and_run_tests.sh
+++ build_src/gcpdac/test/build_and_run_tests.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+echo "start build and run tests"
+ls
 cd ../../../
+echo "cd up 3"
+ls
 
 # variables
 image_name=gcr.io/tranquility-base-images/tb-gcp-dac-test-harness:main
@@ -14,4 +18,5 @@ docker build \
  .
 
 ## Spin up
+echo "-- docker-compose --"
 docker-compose -f build_src/gcpdac/test/docker-compose-test.yml up
