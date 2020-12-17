1e9acbac3c1bd864a1125b3c44605cad60911d62
Geoffrey Pell <Geoffrey.Pell@gft.com>
2020-12-17 20:00:40 +0000

    #431 tidy up

--- build_src/gcpdac/test/build_and_run_tests.sh
+++ build_src/gcpdac/test/build_and_run_tests.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-echo "start build and run tests"
-ls
 cd ../../../
-echo "cd up 3"
-ls
 
 # variables
 image_name=gcr.io/tranquility-base-images/tb-gcp-dac-test-harness:main
-
-# Run unit tests
-#python3 -m unittest discover -s src/test/python/tranquilitybase/gcpdac/unit/
 
 # Build
 docker build \
@@ -18,5 +11,4 @@ docker build \
  .
 
 ## Spin up
-echo "-- docker-compose --"
 docker-compose -f build_src/gcpdac/test/docker-compose-test.yml up --abort-on-container-exit
