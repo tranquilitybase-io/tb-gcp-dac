be9883cd0c02df4f56c437015b175d8cebd43c7a
sunderhill-gft <59086962+sunderhill-gft@users.noreply.github.com>
2020-12-17 12:51:56 +0000

    Issue 195 - Fix bug on polling Jenkins job (#196)
    * when polling jenkins, match build check just on job unique id

--- gcpdac/jenkins_utils.py
+++ gcpdac/jenkins_utils.py
@@ -1,21 +1,21 @@
-from typing import Any, Union, Tuple, Generator
+from typing import Any, Union, Tuple, Generator, Optional
 
 from jenkinsapi.build import Build
 from jenkinsapi.jenkins import Jenkins
 from jenkinsapi.job import Job
 
 import config
-from config import JENKINS_BASE_URL
+from gcpdac.constants import JOB_UNIQUE_ID
 
 logger = config.logger
 
 
 def get_server_instance():
-    server = Jenkins(JENKINS_BASE_URL, username=config.JENKINS_USER, password=config.JENKINS_PASSWORD)
+    server = Jenkins(config.JENKINS_BASE_URL, username=config.JENKINS_USER, password=config.JENKINS_PASSWORD)
     return server
 
 
-def get_job_build(job_name, job_params):
+def get_job_build(job_name, job_params: dict) -> Optional[Build]:
     try:
         jenkins = get_server_instance()
 
@@ -25,22 +25,20 @@ def get_job_build(job_name, job_params):
         logger.debug('Is Job running:%s' % (job_instance.is_running()))
         logger.debug('Is Job enabled:%s' % (job_instance.is_enabled()))
         build_ids = job_instance.get_build_ids()
-        build_by_params: Build = job_instance.get_build_by_params(job_params)
-        if build_by_params != None:
-            logger.debug("Found build by params")
-            return build_by_params
-        else:
-            logger.debug("Didn't find build by params")
+
         for build_id in build_ids:
             logger.debug("build id {}".format(build_id))
             build = job_instance.get_build(build_id)
-            build_params = build.get_params()
+            build_params: dict = build.get_params()
             logger.debug("build params {}".format(build_params))
-            if build_params == job_params:
-                logger.debug("Matched build on params")
+            logger.debug("job params {}".format(job_params))
+            if build_params[JOB_UNIQUE_ID] == job_params[JOB_UNIQUE_ID]:
+                logger.debug("Matched build on job unique id")
                 return build
             else:
-                logger.debug("Build didn't match on params")
+                logger.debug("Could not find a build matching job unique id")
+
+        return None
     except Exception as ex:
         logger.debug("Exception: {0}".format(ex))
 
@@ -55,7 +53,6 @@ def format_jenkins_url(jenkins_params, jenkins_url):
 
 
 def get_all_job_details():
-    # Refer Example #1 for definition of function 'get_server_instance'
     try:
         jenkins = get_server_instance()
         jobs: Generator[Union[Tuple[str, Job], Tuple[Any, Job]], Any, None] = jenkins.get_jobs()
@@ -67,7 +64,6 @@ def get_all_job_details():
             logger.debug('Is Job running:%s' % (job_instance.is_running()))
             logger.debug('Is Job enabled:%s' % (job_instance.is_enabled()))
             build_ids = jenkins[job_name].get_build_ids()
-            # jenkins[job_name].get_build_by_params()
             for build_id in build_ids:
                 logger.debug("build id {}".format(build_id))
                 build = jenkins[job_name].get_build(build_id)
