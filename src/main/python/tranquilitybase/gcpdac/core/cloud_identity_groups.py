03d2c71059acb1776f56225259f55050c015cde9
Geoffrey Pell <Geoffrey.Pell@gft.com>
2020-12-22 12:04:11 +0000

    #189 hardening metadata endpoint

--- src/main/python/tranquilitybase/gcpdac/core/cloud_identity_groups.py
+++ src/main/python/tranquilitybase/gcpdac/core/cloud_identity_groups.py
@@ -10,13 +10,17 @@ logger = get_logger(get_frame_name(inspect.currentframe()))
 
 def read_all():
     logger.debug("read_all")
-    r: Response = requests.get("https://cloudidentity.googleapis.com/v1/groups")
-    logger.debug("response is {} ".format(r))
-    data = r.json()
-    logger.debug("response data is {} ".format(data))
-    groups = data["groups"]
-    for group in groups:
-        logger.debug("group is {} ".format(group))
+    try:
+        r: Response = requests.get("https://cloudidentity.googleapis.com/v1/groups")
+        logger.debug("response is {} ".format(r))
+        data = r.json()
+        logger.debug("response data is {} ".format(data))
+        groups = data["groups"]
+        for group in groups:
+            logger.debug("group is {} ".format(group))
 
-    return data
+        return data
+    except Exception as e:
+        logger.debug("error generating meta data {} ".format(e))
+        return 500
 
