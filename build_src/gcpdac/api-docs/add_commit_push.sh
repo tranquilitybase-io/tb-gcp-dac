24a9c9b9a8a31a70857e35621ba096047b19f5e9
Geoffrey Pell <Geoffrey.Pell@gft.com>
2020-12-23 10:46:18 +0000

    #189 debugging workflow

--- build_src/gcpdac/api-docs/add_commit_push.sh
+++ build_src/gcpdac/api-docs/add_commit_push.sh
@@ -1,15 +1,10 @@
 # === git add, commit and push ===
-echo "--- hello ---"
-ls
-
-
 git config user.email git.action@gft.com
 git config user.name "gitaction gft"
 
 # check if file changed
-echo "--- status ---"
 git status
-if ! git diff --no-ext-diff --quiet --exit-code docs/index.html; then
+if ! git diff --no-ext-diff --quiet --exit-code ../../../docs/index.html; then
   git add ../../../docs/index.html
   git commit -m "Add automatically generated openapi static HTML documentation"
 fi
