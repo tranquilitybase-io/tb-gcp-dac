86e1906bdce74c9ec483fb8dc8f2fc25a9f838d5
Geoffrey Pell <Geoffrey.Pell@gft.com>
2020-12-23 10:43:19 +0000

    #189 debugging workflow

--- build_src/gcpdac/api-docs/generate_docs.sh
+++ build_src/gcpdac/api-docs/generate_docs.sh
@@ -1,7 +1,13 @@
 # === Install redoc-cli and generate API documentation ===
+cd ../../../
+ls
+echo "?????"
+
+
+
 npm install redoc-cli
-npx redoc-cli bundle ../../../src/main/python/tranquilitybase/gcpdac/openapi.yml \
+npx redoc-cli bundle src/main/python/tranquilitybase/gcpdac/openapi.yml \
  --title "GCP DAC service REST API" \
- --output ../../../docs/index.html
+ --output docs/index.html
 rm -fr node_modules
 rm -fr package-lock.json
