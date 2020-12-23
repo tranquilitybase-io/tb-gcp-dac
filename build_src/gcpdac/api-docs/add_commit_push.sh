8f4dfc879b66818166f049498d1a07a55d533610
Geoffrey Pell <Geoffrey.Pell@gft.com>
2020-12-23 10:34:33 +0000

    #189 debugging workflow

--- build_src/gcpdac/api-docs/add_commit_push.sh
+++ build_src/gcpdac/api-docs/add_commit_push.sh
@@ -1,8 +1,10 @@
 # === git add, commit and push ===
+echo "--- hello ---"
 git config user.email git.action@gft.com
 git config user.name "gitaction gft"
 
 # check if file changed
+echo "--- status ---"
 git status
 if ! git diff --no-ext-diff --quiet --exit-code docs/index.html; then
   git add ../../../docs/index.html
