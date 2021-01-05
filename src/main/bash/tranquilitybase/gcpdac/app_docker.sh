b69b3fe806ffb722abaf4c6a2340bd584e68fa6a
Geoffrey Pell <Geoffrey.Pell@gft.com>
2021-01-05 16:21:55 +0000

    #189 debugging GKE config

--- src/main/bash/tranquilitybase/gcpdac/app_docker.sh
+++ src/main/bash/tranquilitybase/gcpdac/app_docker.sh
@@ -3,6 +3,8 @@ LOGLEVEL=debug
 NUMBER_OF_WORKERS=1
 PORT=3100
 
+exit 222
+
 if [ ! -z "${1}" ]; then
   PORT=${1}
 fi
@@ -16,5 +18,5 @@ gunicorn app:connex_app \
 --workers="${NUMBER_OF_WORKERS}" \
 --bind="0.0.0.0:${PORT}" \
 --log-level="${LOGLEVEL}" \
---access-logformat '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"' \
---chdir src/main/python/tranquilitybase/gcpdac/
+--access-logformat '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+#--chdir src/main/python/tranquilitybase/gcpdac/
