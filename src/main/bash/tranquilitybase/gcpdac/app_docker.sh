6dc4c9662dfee2e7589f0491f5454b697f60f78b
Geoffrey Pell <Geoffrey.Pell@gft.com>
2021-01-06 10:42:25 +0000

    #189 debugging GKE config

--- src/main/bash/tranquilitybase/gcpdac/app_docker.sh
+++ src/main/bash/tranquilitybase/gcpdac/app_docker.sh
@@ -15,20 +15,25 @@ echo "Using port: ${PORT}"
 log_dir='/var/log/tb-gcp-dac.log'
 APP_PORT="${PORT}"
 
-if [[ -d "src/main/python/tranquilitybase/gcpdac/" ]]
+
+if [ -d src/main/python/tranquilitybase/gcpdac/ ]
 then
-    printf "This file exists on your filesystem."
+    printf "This file exists!!! YAYAYAYA. :]"
     exit 201
 else
-    printf "no file."
+    printf "no file. NO :["
     exit 202
 fi
 
 exit 10
-
-gunicorn app:connex_app \
---workers="${NUMBER_OF_WORKERS}" \
---bind="0.0.0.0:${PORT}" \
---log-level="${LOGLEVEL}" \
---access-logformat '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
-#--chdir src/main/python/tranquilitybase/gcpdac/
+{
+  gunicorn app:connex_app \
+  --workers="${NUMBER_OF_WORKERS}" \
+  --bind="0.0.0.0:${PORT}" \
+  --log-level="${LOGLEVEL}" \
+  --access-logformat '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"' \
+  --chdir src/main/python/tranquilitybase/gcpdac/
+} || {
+  echo "what"
+  exit 20
+}
