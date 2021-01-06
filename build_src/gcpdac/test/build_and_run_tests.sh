30c77734a7df4c99e66002a4e558e657154ae209
Geoffrey Pell <Geoffrey.Pell@gft.com>
2021-01-06 15:44:24 +0000

    #189 fixing failing test

--- build_src/gcpdac/test/build_and_run_tests.sh
+++ build_src/gcpdac/test/build_and_run_tests.sh
@@ -4,11 +4,14 @@ cd ../../../
 # variables
 image_name=gcr.io/tranquility-base-images/tb-gcp-dac-test-harness:main
 
+
+
+
 # Build
 docker build \
   -t $image_name \
   -f build_src/gcpdac/test/Dockerfile_test \
  .
 
-## Spin up
+# Spin up
 docker-compose -f build_src/gcpdac/test/docker-compose-test.yml up --abort-on-container-exit
